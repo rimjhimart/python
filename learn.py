0b441f94ae9c5f36368f9e8c050584a4cac87701
rimjhimart <rimjhim.gupta.ak@gmail.com>
2025-01-24 16:13:46 +0530

    Madlibs Game

--- learn.py
+++ learn.py
@@ -8,5 +8,5 @@ adjective3 = input("enter an adjective (description): ")
 
 print(f"Today I went to a {adjective1} zoo.")
 print(f"In an exhibit, i saw a {noun1}")
-print(f"{noun1} was {adjective2} and {verb1}")
+print(f"{noun1} was {adjective2} and {verb1}") 
 print(f"I was {adjective3}")
