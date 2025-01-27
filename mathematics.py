47225e827996ef67a6b907f38d2ccb971fc01ca5
rimjhimart <rimjhim.gupta.ak@gmail.com>
2025-01-27 23:48:33 +0530

    equations in python

---
+++ mathematics.py
@@ -0,0 +1,17 @@
+
+import math
+
+#area of circle
+
+# radius = float(input("What's the radius of circle?: "))
+#area = math.pi * pow(radius, 2)
+# print(f"The area of your circle is {area:.2f}cm^2")
+
+
+# pythagorus theorem
+
+a = float(input("Base of the triangle: "))
+b = float(input("perpendicular length of the triangle: "))
+c = math.sqrt(pow(a,2)+pow(b,2))
+
+print(f" the hypotenuos of the triangle is {c:.2f}cm")
