fe6ed83591adb1b0168c09d23f77f9164680d769
rimjhimart <rimjhim.gupta.ak@gmail.com>
2025-02-16 22:59:04 +0530

    learning python

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+fruits = ["orange", "apple", "banana", "coconut"]
+# print(dir(fruit))
+# print(help(fruit))
+# print(len(fruit))
+
+# print("banana" in fruit)
+# fruits[-1] = 'pineapple'
+# print("pineapple" in fruits)
+
+# fruits.append("pineapple")
+# fruits.insert(0, "cherry")
+# fruits.sort()
+# fruits.reverse()
+# for fruit in fruits:
+#      print(fruit, end=" ")
+print(fruits.index("coconut"))
+print(fruits)
