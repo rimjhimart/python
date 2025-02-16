7fa84d8de0bac63d50b0c923aa0636d417fe34c1
rimjhimart <rimjhim.gupta.ak@gmail.com>
2025-02-16 22:58:10 +0530

    digital timer

---
+++ digitalclock.py
@@ -0,0 +1,10 @@
+import time
+my_time = int(input("Enter the Time in Seconds: "))
+for x in range(my_time,0,-1):
+    second = x % 60
+    minutes = int(x/60)%60
+    hours = int(x/3600)
+    print(f"{hours:02}:{minutes:02}:{second:02}")
+    time.sleep(1)
+
+print("Goal Achieved!")
