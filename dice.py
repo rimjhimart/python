74aa56fae7607905d2c61c7d43145ee6e6c2f28d
rimjhimart <rimjhim.gupta.ak@gmail.com>
2025-02-21 13:46:59 +0530

    rolling dice

---
+++ dice.py
@@ -0,0 +1,51 @@
+import random
+
+# ●   ┌   ─   ┐   │   └   ┘
+
+dice_art = {
+    1: ("┌─────────┐", 
+         "│         │", 
+         "│    ●    │", 
+         "│         │", 
+         "└─────────┘"),
+                  2:("┌─────────┐",
+                      "│●        │",
+                      "│         │",
+                      "│       ● │",
+                      "└─────────┘"),
+                  3:("┌─────────┐",
+                      "│ ●       │",
+                      "│    ●    │",
+                      "│        ●│",
+                      "└─────────┘"),
+                  4:("┌─────────┐",
+                      "│ ●    ●  │",
+                      "│         │",
+                      "│ ●    ●  │",
+                      "└─────────┘"),
+                  5:("┌─────────┐",
+                      "│ ●    ●  │",
+                      "│    ●    │",
+                      "│ ●    ●  │",
+                      "└─────────┘"),
+                  6:("┌─────────┐",
+                      "│ ●     ● │",
+                      "│ ●     ● │",
+                      "│ ●     ● │",
+                      "└─────────┘")}
+
+dice = []
+total = 0
+num_dice = int(input("How many dice?: "))
+for die in range(num_dice):
+    dice.append(random.randint(1,6))
+
+for die in range(num_dice):
+    for line in dice_art.get(dice[die]):
+        print(line)
+
+for die in dice:
+    total += die
+    print(f"total: {total}")
+
+
